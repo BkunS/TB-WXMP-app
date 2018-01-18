f111e45115c46b8073d348cb2fd74a151573ae8b
Bokun Sun <bsun@toryburch.com>
2018-01-18 16:43:26 -0500

    wxmp-app working in progress

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    apiBaseUrl: 'http://localhost:10010'
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,40 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World!',
+    landingContents: {},
+    categories:[],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    const page = this;
+
+    wx.request({
+      method: 'GET',
+      url: app.globalData.apiBaseUrl + '/v1/landingContents',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        page.setData({
+          landingContents: res.data
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    })
+
+    wx.request({
+      method: 'GET',
+      url: app.globalData.apiBaseUrl + '/v1/categories', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: (res) => {
+        page.setData({
+          categories: res.data
+        })
+      }
     })
   }
 })
